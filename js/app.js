4470d92f16405aba136f0bd4849c7046491e896c
Abi Vickery <vickeryabi@gmail.com>
2019-10-02 15:52:48 -0500

    var
    making variables

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
-/*
-Hero's Journey
-The hero is on an adventure.
-They must fight three enemies along the way, who have 50 points of health each.
-They will be on the brink of death with a health level of 20 out of 100, mana of 0 out of 60, and an energy of 40 out of 100.
-They will meet an man who happens to be a healer along the way that will sell them a healing potion for 10 gold coins, that restores their health back to full strength.
-The hero will need to rest for 3hrs to restore their mana and energy.
-The hero will battle a giant spider with a sword that does 30 points of damage, as well as a eldritch blast that does 20 points of damage.
-The giant spider has a health level of 250 and does 30 points of damage per attack.
-Dodging the giant spider's attack will require 20 points of energy.
-When the hero defeats the giant spider they will be rewarded 100 gold coins.
-*/
+
+// Hero's Journey
+// The hero is on an adventure.
+var hero;
+var heroHealth;
+var heroMana;
+var heroEnergy;
+// They must fight three enemies along the way, who have 50 points of health each.
+var enemy;
+let enemy = 50;
+// They will be on the brink of death with a health level of 20 out of 100, mana of 0 out of 60, and an energy of 40 out of 100.
+let heroHealth = 100;
+let heroMana = 60;
+let heroEnergy = 100;
+// They will meet an man who happens to be a healer along the way that will sell them a healing potion for 10 gold coins, that restores their health back to full strength.
+// The hero will need to rest for 3hrs to restore their mana and energy.
+// The hero will battle a giant spider with a sword that does 30 points of damage, as well as a eldritch blast that does 20 points of damage.
+// The giant spider has a health level of 250 and does 30 points of damage per attack.
+// Dodging the giant spider's attack will require 20 points of energy.
+// When the hero defeats the giant spider they will be rewarded 100 gold coins.
+
+
+
+
+let hero = 100;
