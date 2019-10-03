e4308cd0bb9015eefaf00a092cc40f6be9173ad6
Abi Vickery <vickeryabi@gmail.com>
2019-10-03 15:51:53 -0500

    psuedo mistakes
    fixing mistakes in the psuedo code and spelling ( i can't belive i misspelled the word hero

--- js/app.js
+++ js/app.js
@@ -1,39 +1,41 @@
 
 // Hero's Journey
 // The hero is on an adventure.
-var health;
+// var health;
 let health = 100;
 var hero;
-var heroHealth;
-var heroMana;
-var heroEnergy;
+// var heroHealth;
+// var heroMana;
+// var heroEnergy;
 // They must fight three enemies along the way, who have 50 points of health each.
-var enemy;
+// var enemy;
 let enemy = 50;
 // They will be on the brink of death with a health level of 20 out of 100, mana of 0 out of 60, and an energy of 40 out of 100.
 let heroHealth = 100;
 let heroMana = 60;
 let heroEnergy = 100;
 // They will meet an man who happens to be a healer along the way that will sell them a healing potion for 10 gold coins, that restores their health back to full strength.
-var coin;
+// var coin;
 let coin = 1;
-var gold;
+// var gold;
 let gold = 50;
-var goldCoin;
+// var goldCoin;
 let goldCoin = 1 * 50;
 // The hero will need to rest for 3hrs to restore their mana and energy.
-var hour;
+// var hour;
 let hour = 1;
 // The hero will battle a giant spider with a sword that does 30 points of damage, as well as a eldritch blast that does 20 points of damage.
-var spider;
+// var spider;
 let spider = health * 2 + 50;
 // The giant spider has a health level of 250 and does 30 points of damage per attack.
-var damage;
+// var damage;
 let damage = 10;
-let spider = damage * 2;
+let spiderDamage = damage * 2;
+// let spider = damage * 2;
 // Dodging the giant spider's attack will require 20 points of energy.
 // When the hero defeats the giant spider they will be rewarded 100 gold coins.
 
+console.log(coin);
 
 
 
